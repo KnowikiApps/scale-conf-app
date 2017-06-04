9c5c67a1b2af45632847f16228e47d25b49a06be
Derek Nowicki <derek@knowikiapps.com>
2017-06-03 23:22:17 -0700

    add functions for adding records to tables

--- js/database.js
+++ js/database.js
@@ -23,3 +23,47 @@ function create_tables(){
         console.log("Error creating table in database: " + err)
     };
 }
+
+/*
+  Add a record to a database table
+  @param - table_name - string with table name
+  @param - json_data - JSON object formatted with `column_name: data`
+*/
+function add_record(table_name, json_data){
+    var db = connect_db("ScalConf", "1.0", "Scale Conference App", 1000000);
+    try{
+        db.transaction(function(tx){
+            tx.executeSql("INSERT INTO "+ table_name + get_sql(json_data));
+        })
+    }catch(err){
+        console.log("Error inserting record: " + err);
+    };
+}
+
+/*
+  Helper function for turning a json object into columns and values syntax.
+  JSON should be formated like: column_name: "value"
+*/
+function get_sql(json_object){
+    var cols = "(";
+    var vals = "VALUES (";
+    for(var x in json_object){
+        cols = cols.concat("'", x, "', ");
+        vals = vals.concat("'", json_object[x], "', ");
+    }
+
+    cols = cols.slice(0, cols.length - 2) + ") ";
+    vals = vals.slice(0, vals.length - 2) + ")";
+    return cols + vals;
+}
+
+
+
+
+
+
+
+
+
+
+

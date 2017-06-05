2c1c2e6b5b313c820992f68d08f0d0518c9f0f62
Derek Nowicki <derek@knowikiapps.com>
2017-06-05 14:08:24 -0700

    in progress changes before merge

--- js/feed.js
+++ js/feed.js
@@ -8,12 +8,54 @@ function get_feed() {
     //declare callback function for onreadystatechange event
     doc.onreadystatechange = function(){
         if(doc.readyState === XMLHttpRequest.HEADERS_RECEIVED){
-            console.log(doc.getAllResponseHeaders());
+//            console.log(doc.getAllResponseHeaders());
         }else if(doc.readyState === XMLHttpRequest.DONE){
-            console.log(doc.responseText);//print xml text to console
+//            console.log(doc.responseText);//print xml text to console
+            update_db(doc);
         }
     }
 
     doc.open("GET", "https://www.socallinuxexpo.org/scale/15x/sign.xml");
     doc.send();
 }
+
+function update_db(xml){
+    var xmlDoc = xml.responseXML;
+    var nodes = xmlDoc.documentElement.childNodes;
+    for(var i = 0; i < nodes.length; i++){
+        var json_data = {};
+        for(var j = 0; j < nodes[i].childNodes.length; j++){
+            if (nodes[i].childNodes[j].nodeType !== 3 && (typeof nodes[i].childNodes[j].childNodes[0] !== 'undefined')){
+                var node_name = nodes[i].childNodes[j].nodeName;
+                var node_value = nodes[i].childNodes[j].childNodes[0].nodeValue;
+//                console.log("ChildNode -> name: " + node_name + "| value: " + node_value);
+                json_data[node_name] = node_value;
+                console.log(JSON.stringify(json_data));
+                console.log("*************************************")
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/initialize.js
+++ js/initialize.js
@@ -3,8 +3,11 @@ Javascript library for initializing components used by the application
 */
 
 .import "feed.js" as Feed
+.import "database.js" as Database
 
 //startup script for when the app first loads
 function startup(){
     Feed.get_feed();//get event feed xml file
+//    Database.create_tables();
+//    Database.add_record("speakers", {first:"doug", last:"johnson", company:"Balls Corp", position:"fondler", bio:"doug loves balls"});
 }

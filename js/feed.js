60e448cc172b619b4c54db2e51c359e048fca6a9
Derek Nowicki <derek@knowikiapps.com>
2017-11-03 10:30:31 -0700

    clean up fetching sign data

--- js/feed.js
+++ js/feed.js
@@ -1,48 +1,23 @@
+.import "database.js" as Database
+
 /*
 Library for handling the conference event feed
 */
-
 function get_feed() {
     var doc = new XMLHttpRequest();
 
     //declare callback function for onreadystatechange event
     doc.onreadystatechange = function(){
         if(doc.readyState === XMLHttpRequest.HEADERS_RECEIVED){
-//            console.log(doc.getAllResponseHeaders());
-        }else if(doc.readyState === XMLHttpRequest.DONE){
-//            console.log(doc.responseText);//print xml text to console
-            update_db(doc);
+            console.log(doc.getAllResponseHeaders());
+        }else if(doc.readyState === XMLHttpRequest.DONE && doc.responseText.length > 0){
+            Database.add_xml(doc.responseText); //add feed xml to database
         }
     }
 
     doc.open("GET", "https://www.socallinuxexpo.org/scale/15x/sign.xml");
     doc.send();
 }
-
-function update_db(xml){
-    var xmlDoc = xml.responseXML;
-    var nodes = xmlDoc.documentElement.childNodes;
-    for(var i = 0; i < nodes.length; i++){
-        var json_data = {};
-        for(var j = 0; j < nodes[i].childNodes.length; j++){
-            if (nodes[i].childNodes[j].nodeType !== 3 && (typeof nodes[i].childNodes[j].childNodes[0] !== 'undefined')){
-                var node_name = nodes[i].childNodes[j].nodeName;
-                var node_value = nodes[i].childNodes[j].childNodes[0].nodeValue;
-//                console.log("ChildNode -> name: " + node_name + "| value: " + node_value);
-                json_data[node_name] = node_value;
-                console.log(JSON.stringify(json_data));
-                console.log("*************************************")
-            }
-        }
-    }
-}
-
-
-
-
-
-
-
 
 
 
--- js/initialize.js
+++ js/initialize.js
@@ -7,7 +7,5 @@ Javascript library for initializing components used by the application
 
 //startup script for when the app first loads
 function startup(){
-    Feed.get_feed();//get event feed xml file
-//    Database.create_tables();
-//    Database.add_record("speakers", {first:"doug", last:"johnson", company:"Balls Corp", position:"fondler", bio:"doug loves balls"});
+    Database.create_tables();
 }

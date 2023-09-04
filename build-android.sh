40d5a279e870bc2654f2806a75963c1e420f9e78
derek nowicki <derek@knowikiapps.com>
2023-09-04 00:20:09 -0700

    android build step command

--- build-android.sh
+++ build-android.sh
@@ -6,4 +6,4 @@ cd /work/build
 
 /Qt/5.15.2/android/bin/qmake ../scale-conf-docker.pro -spec android-clang -Wall
 
-# /usr/lib/android-sdk/ndk/21.3.6528147/prebuilt/linux-x86_64/bin/make qmake_all
+/usr/lib/android-sdk/ndk/21.3.6528147/prebuilt/linux-x86_64/bin/make armeabi-v7a-all

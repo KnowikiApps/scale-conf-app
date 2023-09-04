e96b063ffc64cb42e7460698c43b6b9d77482eb4
derek nowicki <derek@knowikiapps.com>
2023-09-03 22:39:35 -0700

    create build dir and qmake inside

--- build-android.sh
+++ build-android.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-/Qt/5.15.2/android/bin/qmake scale-conf-docker.pro -spec android-clang -Wall
+mkdir /work/build
+
+cd /work/build
+
+/Qt/5.15.2/android/bin/qmake ../scale-conf-docker.pro -spec android-clang -Wall
 
 # /usr/lib/android-sdk/ndk/21.3.6528147/prebuilt/linux-x86_64/bin/make qmake_all
